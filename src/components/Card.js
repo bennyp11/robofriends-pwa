137102c4d9c241de21bea311e2d4e335b967ef58
Ben Pollock <ben.r.pollock@gmail.com>
2022-02-21 12:58:17 -0800

    moving heights to 350x800 for PWA high resolution requirement

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 const Card = ({ name, email, id }) => {
     return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5' class='.card'>
-        <img alt='robots' width="604" height="800" src={`https://robohash.org/${id}?size=604x800`} />
+        <img alt='robots' width="350" height="800" src={`https://robohash.org/${id}?size=350x800`} />
         <div>
             <h2>{name}</h2>
             <p>{email}</p>
